e11e01a997b9d6da030e85214a3a95d297790896
takuto <takuto@takutonoMacBook-Air.local>
2024-05-18 16:55:50 +0900

    身体情報登録画面の作成

--- app/models/body.rb
+++ app/models/body.rb
@@ -1,4 +1,10 @@
 class Body < ApplicationRecord
   belongs_to :user
   has_one :goal, dependent: :destroy
+  validates :sex, presence: true, numericality: {in: 1..2}
+  validates :age, presence: true, numericality: true
+  validates :weight, presence: true, numericality: true
+  validates :height, presence: true, numericality: true
+
+  enum sex: {male: 1,famale: 2}
 end
