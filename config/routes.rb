a37cd99e428048f8b696ad677d77b18561ffd148
takuto <takuto@takutonoMacBook-Air.local>
2024-06-24 22:30:43 +0900

    updateアクションの調整

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       resources :goals 
     end
   end
-  resources :recipes, only: %i[new create update]
+  resources :recipes, only: %i[new create update show]
   get '/recipes/result', to: 'recipes#result', as: 'recipes_result'
   get '/recipes/chat', to: 'recipes#chat', as: 'recipes_chat'
   get '/recipes/details', to: 'recipes#details', as: 'recipes_details'
