803955b66f6895a9318bec2daebe80e027601689
takuto <takuto@takutonoMacBook-Air.local>
2024-06-29 17:03:59 +0900

    byebugの設定

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,18 +10,17 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @individual_id = @recipe.individual_id
     if Recipe.exists?(individual_id: @recipe.individual_id)
       @recipe = Recipe.find_by(individual_id: @recipe.individual_id)
       redirect_to recipe_path(id: @recipe.id)
     else 
       agent = Mechanize.new
-      page = agent.get(@individual_id)
+      page = agent.get(@recipe.individual_id)
       @ingredients = page.search(".name").map(&:text)
       @amounts = page.search(".amount").map(&:text)
       @combined = @ingredients.zip(@amounts).map{ |pair| pair.join(": ") }
 
-      combined_text = params[:@combined].join(",")
+      combined_text = @combined.join(",")
       chat_api = OpenAi::ChatApi.new("totalcalories,proteins,carbhydrates,fats,salts,fibersの数値だけ答えてください。各項目の前に[,]つけて。例: 100,200,300,400,500,600")
       chat_responce = chat_api.chat(combined_text)
       @combined = chat_responce.split(",")
@@ -29,7 +28,7 @@ class RecipesController < ApplicationController
         individual_id: @individual_id,
         calories: @combined[0],
         proteins: @combined[1],
-        carbhydrates: @combined[2],
+        carbhydarates: @combined[2],
         fats: @combined[3],
         salts: @combined[4],
         fibers: @combined[5])
