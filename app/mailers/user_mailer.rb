9bc9650e571262198c5d8c3a96bc241f8c05b6b0
takuto <takuto@takutonoMacBook-Air.local>
2024-07-07 23:09:24 +0900

    openletterwebの設定

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'from@example.com'
-
-  def reset_password_email(user)
+def reset_password_email(user)
     @user = User.find(user.id)
     @url  = edit_password_reset_url(@user.reset_password_token)
     mail(to: user.email,
