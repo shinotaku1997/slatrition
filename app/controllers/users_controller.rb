36f88019c9f509bad0c67bc3410dd487e08f05b0
takuto <takuto@takutonoMacBook-Air.local>
2024-07-07 14:34:21 +0900

    auto_loginの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      auto_login(@user)
       flash[:success]= "アカウント登録に成功しました。"
       redirect_to new_user_body_path(@user)
     else
